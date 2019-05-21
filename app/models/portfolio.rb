4436a42e359ecc87888456296887fdcf695793e8
debo9210 <omololuadebowale1@gmail.com>
2019-05-21 11:24:38 +0200

    Impemented image defaults for portfolio items

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -6,4 +6,11 @@ class Portfolio < ApplicationRecord
   end
 
   scope :angular, -> {where(subtitle: "Angular")}
+
+  after_initialize :set_defaults
+
+  def set_defaults
+    self.main_image ||= "https://placehold.it/600x400"
+    self.thumb_image ||= "https://placehold.it/350x200"
+  end
 end
