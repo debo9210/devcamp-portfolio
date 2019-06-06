47c3cd0c2dfeb0b559a0c65ce9ad4e14608efc74
debo9210 <omololuadebowale1@gmail.com>
2019-06-06 13:39:04 +0200

    Integrated initial drag and drop implementation

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,12 +11,11 @@
 // about supported directives.
 //
 //= require rails-ujs
-//= require activestorage
-//= require_tree .
 //= require jquery3
-//= require popper
 //= require jquery-ui
+//= require popper
+//= require bootstrap-sprockets
+//= require activestorage
 //= require html.sortable
 //= require turbolinks
-//= require bootstrap-sprockets
-
+//= require_tree .
