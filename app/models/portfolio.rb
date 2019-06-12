7f258cc94cbb60f3d0a31e872068c583e29a2e8c
debo9210 <omololuadebowale1@gmail.com>
2019-06-12 11:13:05 +0200

    Updated file upload forms and and refactored placeholder functionality into view

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -5,9 +5,8 @@ class Portfolio < ApplicationRecord
   # has_one_attached :main_image
   # has_one_attached :thumb_image                               
 
-  include Placeholder
-  validates_presence_of :title, :body, :main_image, :thumb_image
-
+  validates_presence_of :title, :body
+  
   mount_uploader :thumb_image, PortfolioUploader
   mount_uploader :main_image, PortfolioUploader
 
@@ -21,10 +20,4 @@ class Portfolio < ApplicationRecord
 
   scope :angular, -> {where(subtitle: "Angular")}
 
-  after_initialize :set_defaults
-
-  def set_defaults
-    self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
-    self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200')
-  end
 end
