df43fdcd9acaa63c2daeeb8cc48f20a01ce18d55
debo9210 <omololuadebowale1@gmail.com>
2019-06-28 11:45:45 +0200

    Updated secret key base call

--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,6 @@ module DevcampPortfolio
     #config.action_controller.permit_all_parameters = true
     config.eager_load_paths << "#{Rails.root}/lib"
     #config.eager_load_paths << Rails.root.join('lib')
+    config.secret_key_base = ENV["SECRET_KEY_BASE"]
   end
 end
